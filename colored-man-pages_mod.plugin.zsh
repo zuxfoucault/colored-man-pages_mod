6a01b132b2c34e8da7a410c45f44d213444de869
zuxfoucault <zuxfoucault@gmail.com>
2016-12-22 12:17:39 +0800

    initf

--- colored-man-pages_mod.plugin.zsh
+++ colored-man-pages_mod.plugin.zsh
@@ -1,1 +1,32 @@
-/Users/zuxfoucault/dotfiles/zsh/colored-man-pages_mod.plugin.zsh
+if [[ "$OSTYPE" = solaris* ]]
+then
+	if [[ ! -x "$HOME/bin/nroff" ]]
+	then
+		mkdir -p "$HOME/bin"
+		cat > "$HOME/bin/nroff" <<EOF
+#!/bin/sh
+if [ -n "\$_NROFF_U" -a "\$1,\$2,\$3" = "-u0,-Tlp,-man" ]; then
+	shift
+	exec /usr/bin/nroff -u\$_NROFF_U "\$@"
+fi
+#-- Some other invocation of nroff
+exec /usr/bin/nroff "\$@"
+EOF
+		chmod +x "$HOME/bin/nroff"
+	fi
+fi
+
+man() {
+	env \
+		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+		LESS_TERMCAP_md=$(printf "\e[1;35m") \
+		LESS_TERMCAP_me=$(printf "\e[0m") \
+		LESS_TERMCAP_se=$(printf "\e[0m") \
+		LESS_TERMCAP_so=$(printf "\e[4;36m") \
+		LESS_TERMCAP_ue=$(printf "\e[0m") \
+		LESS_TERMCAP_us=$(printf "\e[3;34m") \
+		PAGER="${commands[less]:-$PAGER}" \
+		_NROFF_U=1 \
+		PATH="$HOME/bin:$PATH" \
+			man "$@"
+}
